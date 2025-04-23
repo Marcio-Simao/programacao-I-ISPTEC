029ce6fe13b9ad5f620810d5b69fb6107afff745
Marcio <marciosimao0825@gmail.com>
2025-04-23 16:22:53 +0100

    novos exercicios

--- lista 5/ex042-05/ex042-05.c
+++ lista 5/ex042-05/ex042-05.c
@@ -8,3 +8,27 @@ struct data
    char mes[30];
    int dia;   
 };
+
+typedef data data;
+
+struct carro 
+{
+   char marca[15];
+   data data;
+   char cor[10];
+   float preco;
+};
+
+typedef carro carro;
+
+//aeron smith
+/*
+   o tipo de estrutura da estruct carro é 
+   estrutura de estrutura são aqueles em 
+   que pelo menos um campo da estrutura é 
+   uma variavel de uma estrutura 
+*/
+
+int main(){
+
+}
