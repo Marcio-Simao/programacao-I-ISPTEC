cf69fe79f098a6b37f2f989a08fa414b80864a5c
Marcio <marciosimao0825@gmail.com>
2025-04-25 22:51:07 +0100

    Primeira prova parcelar

---
+++ provas/PP1/PP1-ex02/PP1-ex02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    char letra[1];
+
+    printf("Digite uma letra: ");
+    scanf("%s", letra[1]);
+
+    if (letra[1] == "B")
+    {
+        printf("Ele vive");
+    }
+    
+}
