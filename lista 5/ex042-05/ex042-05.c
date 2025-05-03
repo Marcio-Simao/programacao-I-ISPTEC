9549561912ae45abd7b77578bccb5783165259e4
Marcio <marciosimao0825@gmail.com>
2025-05-03 20:39:26 +0100

    lista 5

--- lista 5/ex042-05/ex042-05.c
+++ lista 5/ex042-05/ex042-05.c
@@ -14,21 +14,39 @@ typedef data data;
 struct carro 
 {
    char marca[15];
-   data data;
    char cor[10];
    float preco;
+   data data;
 };
 
 typedef carro carro;
 
 //aeron smith
 /*
-   o tipo de estrutura da estruct carro é 
-   estrutura de estrutura são aqueles em 
-   que pelo menos um campo da estrutura é 
-   uma variavel de uma estrutura 
+   o tipo de estrutura da estruct carro é estrutura de estrutura são aqueles em 
+   que pelo menos um campo da estrutura é uma variavel de uma estrutura 
 */
-
+carro vet_carro[20];
 int main(){
+
+   printf("\nREGISTRAR CARRO\n");
+   
+   for (int i = 0; i < 20; i++)
+   {
+      printf("\nMarca: ");
+      scanf("%s", vet_carro[i].marca);
+
+      printf("\nCor: ");
+      scanf("%s", vet_carro[i].cor);
+
+      printf("Preço: ");
+      scanf("%.3f", vet_carro[i].preco);
+
+      printf("");
+      scanf("");
+   }
+       
+   
+   
 
 }
