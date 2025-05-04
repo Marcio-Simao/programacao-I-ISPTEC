a9db6d26f5ca21b7c0f4886b896b371705bbdbc7
Marcio <marciosimao0825@gmail.com>
2025-05-04 18:04:36 +0100

    lista 5

--- lista 5/ex042-05/ex042-05.c
+++ lista 5/ex042-05/ex042-05.c
@@ -26,21 +26,24 @@ typedef struct carro carro;
    o tipo de estrutura da estruct carro é estrutura de estrutura são aqueles em 
    que pelo menos um campo da estrutura é uma variavel de uma estrutura 
 */
-carro vet_carro[20];
+carro vet_carro[21];
 int main(){
    
    int quant;
    float prec;
 
-   printf(" Quantos carros desejas registrar: ");
-   scanf("%d", quant);
+   printf("\n----------\n");
+   printf("Quantos carros desejas registrar: ");
+   scanf("%d", &quant);
+   printf("\n----------\n");
 
    printf("\nREGISTRAR CARRO\n");
    
-   for (int i = 0; i < quant; i++)
+   for (int i = 1; i < quant; i++)
    {
       printf("\n%dº CARRO\n", i);
 
+      printf("\n----------\n");
       printf("\nMarca: ");
       scanf("%s", vet_carro[i].marca);
 
@@ -54,28 +57,31 @@ int main(){
 
       printf("\ndd/mm/aa\n");
       scanf("%d %d %d", &vet_carro[i].data.dia, &vet_carro[i].data.mes, &vet_carro[i].data.ano);
-     
+      printf("\n----------\n");
    }
 
-   printf("Digite o preço: ");
+   printf("\n----------\n");
+   printf("\nDigite a capacidade financeira: ");
    scanf("%f", &prec);
-
-   printf("\nCARROS REGISTADOS\n");
-       
-   for(int i = 0; i < quant; i++){
-
-      if (prec <= vet_carro[i].preco)
-      {      
-      
-      printf("\nMarca: %s \n", vet_carro[i].marca);
-
-      printf("\nCor: %s\n", vet_carro[i].cor); 
-
-      printf("\nPreço: %.3f", vet_carro[i].preco);
-
-      printf("\nData: %d / %d / %d", vet_carro[i].data.dia, vet_carro[i].data.mes, vet_carro[i].data.ano);
-      }   
-   }
+   printf("\n----------\n");
    
+   printf("\nCARROS\n");    
+   for(int i = 0; i < quant; i++)   
+   {
 
+      if (prec >= vet_carro[i].preco)
+      {      
+         printf("\n----------\n");
+
+         printf("\nMarca: %s \n", vet_carro[i].marca);
+
+         printf("\nCor: %s\n", vet_carro[i].cor); 
+
+         printf("\nPreço: %.3f\n", vet_carro[i].preco);
+
+         printf("\nData: %d / %d / %d\n", vet_carro[i].data.dia, vet_carro[i].data.mes, vet_carro[i].data.ano);
+         printf("\n----------\n");
+      }   
+   }   
+   return 0;
 }
