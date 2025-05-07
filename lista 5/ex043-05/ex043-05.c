b18d93ef553deedeba4cc75964b1f6f4bcfb0c21
Marcio <marciosimao0825@gmail.com>
2025-05-07 23:10:41 +0100

    lista 5

--- lista 5/ex043-05/ex043-05.c
+++ lista 5/ex043-05/ex043-05.c
@@ -2,17 +2,18 @@
 #include <string.h>
 #include <locale.h>
 
+
+
 int main () 
 {
 
-    int idade [5] [5], cont;
+    int idade [5] [5], cont = 5;
     for (int i = 0; i < cont; i++)
     {
-        for (int l = 0; l < cont; i++)
+        for (int j = 0; j < cont; i++)
         {
-            
+          printf("idade [%d] [%d]", i, j);
+          scanf("%d", &idade[i] [j]);  
         }
-        
     }
-        
 }
