7718a912fb5902bec17f927f37b6bef15c690c28
Marcio <marciosimao0825@gmail.com>
2025-05-07 10:15:09 +0100

    lista 5

---
+++ lista 5/ex043-05/ex043-05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main () 
+{
+
+    int idade [5] [5], cont;
+    for (int i = 0; i < cont; i++)
+    {
+        for (int l = 0; l < cont; i++)
+        {
+            
+        }
+        
+    }
+        
+}
