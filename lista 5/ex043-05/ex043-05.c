b0dea34ba0616a050d1e603d17972f17bed7de0f
Marcio <marciosimao0825@gmail.com>
2025-05-07 23:41:20 +0100

    lista 5

--- lista 5/ex043-05/ex043-05.c
+++ lista 5/ex043-05/ex043-05.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
-
-
+//definindo uma constante 
+#define cont 5
 
 int main () 
 {
 
-    int idade [5] [5], cont = 5;
+    int idade [5] [5];
+    //armazeneando os valores na matriz
     for (int i = 0; i < cont; i++)
     {
-        for (int j = 0; j < cont; i++)
+        for (int j = 0; j < cont; j++)
         {
           printf("idade [%d] [%d]", i, j);
           scanf("%d", &idade[i] [j]);  
         }
+    }
+    //apresntando os valores da matriz 
+    for(int i = 0; i < cont; i++)
+    {
+        for (int j = 0; j < cont; j++)
+        {
+            printf("idade [%d] [%d]: %d", i, j, idade [i] [j]);
+        }
+        
+
     }
 }
