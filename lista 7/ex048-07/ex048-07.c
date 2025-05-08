6bacc7bc05e7f219fdc82fd9a292f9cc99d5b311
Marcio <marciosimao0825@gmail.com>
2025-05-08 19:25:37 +0100

    lista 7

--- lista 7/ex048-07/ex048-07.c
+++ lista 7/ex048-07/ex048-07.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <locale.h>
 
-struct pessoa 
+//estrutura Pessoa 
+
+struct Pessoa 
 {
     char sexo; 
     float peso;
 };
 
-typedef struct pessoa pessoa;
+//renomeando o estrutura Pessoa
+typedef struct Pessoa pessoa;
 
 int main()
 {
-    pessoa pessoa1
+    //criando variavel do tipo pessoa
+    pessoa pessoa1;
 
+    //lendo os dados 
+    
+    printf("\n--REGISTRO PESSOA--\n");
+    
+    printf("\nSexo(M/F): ");
+    scanf("%c", pessoa1.sexo);
+    
+    printf("Peso: ");
+    scanf("%f", &pessoa1.peso);
+    
+    printf("\n_____________________\n"); 
+    
+    //Apresentando os dados na tela
+    
+    printf("\n--PESSOAS REGISTRADAS--\n");
+    
+    printf("Sexo: %c", pessoa1.sexo);
+    
+    printf("Sexo: %.2", pessoa1.peso);
+    
+    printf("\n_____________________\n");
+    
 }
