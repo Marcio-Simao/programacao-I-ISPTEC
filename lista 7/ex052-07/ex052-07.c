513fa6b7974aaf1f274fdb50d61f61d98197a3a6
Marcio <marciosimao0825@gmail.com>
2025-05-11 20:53:30 +0100

    lista 7

--- lista 7/ex052-07/ex052-07.c
+++ lista 7/ex052-07/ex052-07.c
@@ -22,6 +22,8 @@ typedef struct Estoque Estoque;
 
 void Estoq (Estoque estoque, int cont);
 
+int Cadastrar ( Estoque *cad, int cont);
+
 int main()
 {
     int contador = 0;
@@ -36,7 +38,7 @@ int main()
     do
     {   
         printf("\n--TuDeliciosoGest--\n");
-        printf("\n1-Cadastrar \n");
+        printf("\n1-Cadastrar Produto\n");
         printf("\n2-Registrar Venda\n");
         printf("\n3-Estoque\n");
         printf("\n0-Sair\n");
@@ -45,11 +47,12 @@ int main()
         switch (opcao)
         {
             case '1':
-                /* Cadastrar Venda */
+                /* cadastrar produtos */
+                contador = Cadastrar(&estoque, contador);
             break;
         
             case '2':
-                /* cadastrar produtos */
+                /* Cadastrar Venda */
             break;
 
             case '3':
@@ -88,9 +91,11 @@ void Estoq (Estoque estoque, int cont)
 
         for (int i = 0; i < estoque.total_bebidas; i++)
         {
+            printf("\n-------------\n");
             printf("\nNome: %s\n", estoque.bebidas[i].nome_bebidas);
             
             printf("\nQuantidade: %d\n", estoque.bebidas[i].quant_bebida);
+            printf("\n-------------\n");
 
         }
         
@@ -102,30 +107,42 @@ void Estoq (Estoque estoque, int cont)
 
 int Cadastrar ( Estoque *cad, int cont)
 {   
-    int pao_cad, queijo_cad, tipo_beb;
+    int pao_cad, queijo_cad, tipo_beb, carne_cad;
 
-    printf("--CADASTAR--");
+    printf("\n--CADASTAR--\n");
 
     printf("Pão: ");
     scanf("%d", &pao_cad);
-    cad->pao = pao_cad;
+    cad->pao += pao_cad;
 
     printf("Queijo: ");
     scanf("%d", &queijo_cad);
-    cad->queijo = queijo_cad;
+    cad->queijo += queijo_cad;
+
+    printf("Carne: ");
+    scanf("%d", &carne_cad);
+    cad->carne += carne_cad;
 
     printf("\n--BEBIDAS--\n");
 
     printf("Quantos tipos de bebibas: ");
     scanf("%d", &tipo_beb);
 
+    if (tipo_beb > MAX_BEB) {
+        printf("Erro: máximo de %d tipos de bebidas.\n", MAX_BEB);
+        tipo_beb = MAX_BEB;
+    }
     for (int i = 0; i < tipo_beb; i++)
     {
+        printf("\n-------------\n");
         printf("Nome: ");
-        scanf("%c", cad->bebidas->nome_bebidas);
+        scanf(" %s", cad->bebidas[i].nome_bebidas);
         
         printf("Quantidade: ");
-        scanf("%d", &cad->bebidas->quant_bebida);
+        scanf("%d", &cad->bebidas[i].quant_bebida);
+        printf("\n-------------\n");
     }
-    
+    cad->total_bebidas = tipo_beb;
+
+    return  cont + 1;
 }
