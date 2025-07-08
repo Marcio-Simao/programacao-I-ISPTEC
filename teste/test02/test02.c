91dfad7807fe33141d20a070d1a24b8eccb8ead0
Márcio Simão <marciosimao0825@gmail.com>
2025-07-08 15:09:47 +0100

    teste

--- teste/test02/test02.c
+++ teste/test02/test02.c
@@ -1,5 +1,15 @@
-#include <stdio.h> 
+#include <stdio.h> #include <string.h>
 
-int main(){
-    
+int main()
+{
+    float nota;
+
+    printf("Digite a nota do aluno: ");
+    scanf("%f", &nota);
+
+    if(nota >= 9.5)
+    {
+        printf("\nAprovado!\n");
+    }
+   
 }
