4f3c42cedc4205e2e76b131b11a3deb732a4dfb3
Márcio Simão <marciosimao0825@gmail.com>
2025-07-24 17:12:02 +0100

    teste

---
+++ teste/test10/test10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome[30];
+
+    printf("\nDigite um nome: ");
+    gets(nome);
+
+    printf("\n O seu nome é %s \n\n", nome);
+}
